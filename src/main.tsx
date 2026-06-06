400dc7fdcbc6a46fb2b341210e69d7253d2f76ff
Ryota Oshima <stephon70stehryfg@outlook.com>
2026-06-07 03:29:17 +0900

    changed stack to React.js

--- src/main.tsx
+++ src/main.tsx
@@ -1,53 +1,14 @@
 import ReactDOM from 'react-dom/client'
-import {
-  Outlet,
-  RouterProvider,
-  createRootRoute,
-  createRoute,
-  createRouter
-} from '@tanstack/react-router'
-// removed unused router devtools import
 
 import './styles.css'
 import reportWebVitals from './reportWebVitals.ts'
 
 import App from './app.tsx'
 
-const rootRoute = createRootRoute({
-  component: () => (
-    <>
-      <Outlet />
-    </>
-  )
-})
-
-const indexRoute = createRoute({
-  getParentRoute: () => rootRoute,
-  path: '/',
-  component: App
-})
-
-const routeTree = rootRoute.addChildren([indexRoute])
-
-const router = createRouter({
-  routeTree,
-  context: {},
-  defaultPreload: 'intent',
-  scrollRestoration: true,
-  defaultStructuralSharing: true,
-  defaultPreloadStaleTime: 0
-})
-
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router
-  }
-}
-
 const rootElement = document.getElementById('app')
 if (rootElement && !rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
-  root.render(<RouterProvider router={router} />)
+  root.render(<App />)
 }
 
 reportWebVitals()
